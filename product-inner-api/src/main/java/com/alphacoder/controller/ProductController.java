46455c52fe24c23047ee9e085d7da053ed9dfa72
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-02-02 07:56:44 -0500

    Added add endpoint.

--- product-inner-api/src/main/java/com/alphacoder/controller/ProductController.java
+++ product-inner-api/src/main/java/com/alphacoder/controller/ProductController.java
@@ -1,13 +1,18 @@
 package com.alphacoder.controller;
 
+import com.alphacoder.domain.request.ProductRequest;
 import com.alphacoder.domain.response.ResponseDto;
+import com.alphacoder.domain.response.ResponseStatus;
 import com.alphacoder.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value= "/products")
@@ -28,4 +33,12 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.
                 forSuccess(this.productService.getProducts(page, pageSize)));
     }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseDto> addProduct(@Valid @RequestBody ProductRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(ResponseStatus.SUCCESS, null));
+    }
+
+
 }
--- product-inner-api/src/main/java/com/alphacoder/domain/request/ProductRequest.java
+++ product-inner-api/src/main/java/com/alphacoder/domain/request/ProductRequest.java
@@ -4,12 +4,22 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductRequest {
     private Long id;
+
+    @NotNull(message = "Product name is required.")
+    @NotEmpty(message = "Product name is required.")
     private String name;
+
+    @NotNull(message = "Product description is required.")
+    @NotEmpty(message = "Product description is required.")
     private String description;
+
+    @NotNull(message = "Product price is required.")
     private BigDecimal price;
 }
