b1094162c8dd36a43f89f8b2dfa62a1eb2240029
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-02-07 19:25:35 -0500

    Added spring cloud config code.

--- product-inner-api/src/main/java/com/alphacoder/controller/TestController.java
+++ product-inner-api/src/main/java/com/alphacoder/controller/TestController.java
@@ -1,2 +1,21 @@
-package com.alphacoder.controller;public class TestController {
+package com.alphacoder.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+@RefreshScope
+public class TestController {
+
+    @Value("${app.test}")
+    private String test;
+
+    @GetMapping
+    public String test(){
+        return test;
+    }
 }
