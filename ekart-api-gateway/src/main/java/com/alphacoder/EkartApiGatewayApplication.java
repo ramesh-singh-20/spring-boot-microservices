73ad96bf2ab6aac2be24e0010c71d8e31723f79b
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-02-10 22:03:12 -0500

    Added cloud gateway configs.

--- ekart-api-gateway/src/main/java/com/alphacoder/EkartApiGatewayApplication.java
+++ ekart-api-gateway/src/main/java/com/alphacoder/EkartApiGatewayApplication.java
@@ -1,13 +1,21 @@
 package com.alphacoder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EkartApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EkartApiGatewayApplication.class, args);
 	}
+
 
 }
---
+++ product-inner-api/src/main/java/com/alphacoder/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.alphacoder.config;public class SecurityConfig {
+}
